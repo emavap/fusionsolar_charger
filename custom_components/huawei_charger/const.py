0a80d697e6f9dc489c74458becdff2972c696ee0
Marco Camilli <marco@emavap.be>
2025-04-12 23:03:33 +0200

    .

--- custom_components/huawei_charger/const.py
+++ custom_components/huawei_charger/const.py
@@ -9,18 +9,11 @@ REG_DYNAMIC_POWER_LIMIT = "20001"
 REGISTER_NAME_MAP = {
     REG_FIXED_MAX_POWER: "Fixed Max Charging Power",
     REG_DYNAMIC_POWER_LIMIT: "Dynamic Power Limit",
-    "538976516": "Device IP",
-    "2101260": "Session Energy",
-    "2101259": "Phase A Voltage",
-    "2101261": "Total Energy",
-    "20014": "Charging State",
-    "20016": "Charging Enabled",
+    "538976516": "Device IP",    "2101259": "Phase A Voltage",    "20016": "Charging Enabled",
     "20011": "Device Name",
-    "20010": "Charging Mode",
-    "10035": "A Output Current",
-    "10008": "Total Energy Charged",
+    "20010": "Charging Mode",    "10008": "Total Energy Charged",
     "10001": "Software Version",
     "20013": "Lock Status",
     "20017": "Plugged In",
-    "20029": "Device Serial Number"
+    "20029": "Device Serial Number"    "10007": "Charger Model",
 }
